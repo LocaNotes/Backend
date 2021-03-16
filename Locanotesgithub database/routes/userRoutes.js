a7879ff7aad62eb0e58b650ec45aa38741407021
acomanzo <ansonih17@gmail.com>
2021-03-16 01:37:59 -0400

    created router for user

---
+++ Locanotesgithub database/routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const router = express.Router();
+
+router.post('/user/:firstName/:lastName/:email/:username/:password', userController.user_create);
+
+module.exports = router;
