b1ca213c132688fe3a21755c9154c423db8b39d0
acomanzo <ansonih17@gmail.com>
2021-03-16 02:21:01 -0400

    can create user with query parameters now

---
+++ Locanotesgithub database/controllers/userController.js
@@ -0,0 +1,26 @@
+// const user = require('../models/user');
+const mongoUser = require('../models/user');
+
+const user_create = (req, res) => {
+    const firstName = req.query.firstName;
+    const lastName = req.query.lastName;
+    const email = req.query.email;
+    const username = req.query.username;
+    const password = req.query.password; 
+
+    const user = new mongoUser({firstName: firstName, lastName: lastName, email: email, username: username, password: password});
+
+    console.log('body:');
+    console.log(req.body);
+    user.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+}
+
+module.exports = {
+    user_create
+}
--- Locanotesgithub database/routes/userRoutes.js
+++ Locanotesgithub database/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const router = express.Router();
 
+router.post('/user', userController.user_create);
+
 router.post('/user/:firstName/:lastName/:email/:username/:password', userController.user_create);
 
 module.exports = router;
