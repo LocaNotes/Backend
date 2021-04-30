da082c37306d7d9992ccce39f5a340bac456d19d
Brandon Ozsahin <johncrow02@gmail.com>
2021-04-30 18:22:42 -0400

    added some things for blocked users
    block user same as friend

--- Locanotesgithub database/controllers/blockUserController.js
+++ Locanotesgithub database/controllers/blockUserController.js
@@ -27,7 +27,50 @@ const block_add = (req, res) => {
         });
 }
 
+const block_list = async(req,res) => {
+    const userId = req.params.userId;
+    let blockArray = []; // first array 'one way add'
+    await blockUser.find({userId:userId}).then(result => {
+            for(let i = 0; i < result.length; i++)
+            {
+                blockArray[i] = result[i];
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.send(err);
+        });
+        let blockArray2 = []; // second array of actual blocks i.e. both added
+    for(let k = 0; k < blockArray.length; k++)
+    {
+    await blockUser.find({userId:blockArray[k].BlockedUserId}).then(result => {
+            for(let o = 0; o < result.length; o++)
+            {
+                if(result[o].BlockedUserId === userId){
+                    blockArray2.push(blockArray[k]);
+                }
+            }
+
+        })
+    } res.send(blockArray2);
+
+}
+
+const block_delete = (req, res) => {
+    const userId = req.params.id;
+
+    blockUser.findByIdAndDelete(userId).then(result => {
+        res.send(result);
+    }).catch(err => {
+        console.log(err);
+        res.send(err);
+    });
+}
+
+
 module.exports = {
     blocked_index,
     block_add,
+    block_list,
+    block_delete,
 }
--- Locanotesgithub database/routes/blockUserRoutes.js
+++ Locanotesgithub database/routes/blockUserRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post('/block', blockUserController.block_add);
 router.get('/block', blockUserController.blocked_index);
-
+router.get('/block/:userId',blockUserController.block_list);
+router.delete('block/:userId',blockUserController.block_delete);
 module.exports = router;
