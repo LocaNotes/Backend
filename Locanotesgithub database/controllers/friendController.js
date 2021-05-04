80c633d006a10e17baae703848dab1b4cebff7a8
acomanzo <ansonih17@gmail.com>
2021-05-04 03:02:48 -0400

    friend: support for getting a list of docs that show true friends, getting a list of users that are true friends with another user, removing a friend, checking to see if two users are true friends

--- Locanotesgithub database/controllers/friendController.js
+++ Locanotesgithub database/controllers/friendController.js
@@ -1,4 +1,5 @@
 const friend = require('../models/friend');
+const user = require('../models/user');
 
 const friend_index = (req, res) => {
     friend.find().sort({createdAt: -1}).then(result => {
@@ -26,6 +27,14 @@ const friend_add = (req,res) => {
         });
 }
 
+/**
+ * Sends to the client a list of friend documents where userId is the friender.
+ * Two users are friends iff there is a friend document where user A is the 
+ * friender and user B is the friendee, and another where user B is the friender 
+ * and user A is the friendee.
+ * @param {*} req 
+ * @param {*} res 
+ */
 const friend_list = async(req,res) => {
     const userId = req.params.userId;
     let friendArray = []; // first array 'one way add'
@@ -50,20 +59,81 @@ const friend_list = async(req,res) => {
                 }
             }
 
+        }).catch(err => {
+            res.send(err);
         })
     } res.send(friendArray2);
+}
 
+/**
+ * Same as friend_list, except sends the actual user json, not the friend documents. 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const friend_list_users = async (req, res) => {
+    const userId = req.params.userId;
+
+    // first array 'one way add'
+    let friendsAdded = []; 
+    await friend.find({ userId: userId }).then(result => {
+        for (let i = 0; i < result.length; i++) {
+            friendsAdded[i] = result[i];
+        }
+    }).catch(err => {
+        res.send(err);
+    });
+
+    // second array of actual "friends" i.e. both added
+    let trueFriends = []; 
+    for (let i = 0; i < friendsAdded.length; i++) {
+        await friend.find({ userId: friendsAdded[i].friendUserId }).then(result => {
+            for (let j = 0; j < result.length; j++) {
+                if (result[j].friendUserId === userId) {
+                    trueFriends.push(friendsAdded[i]);
+                }
+            }
+        }).catch(err => {
+            res.send(err);
+        });
+    } 
+
+    // get the actual user documents
+    let friends = [];
+    for (let i = 0; i < trueFriends.length; i++) {
+        const friendeeId = trueFriends[i].friendUserId;
+        await user.find({ _id: friendeeId }).then(result => {
+            if (result.length > 0) {
+                const friend = result[0];
+                friends.push(friend);
+            }
+        }).catch(err => {
+            res.send(err);
+        })
+    }
+
+    res.send(friends);
 }
 
 const friend_delete = (req, res) => {
-    const userId = req.params.id;
+    const frienderId = req.params.frienderId;
+    const friendeeId = req.params.friendeeId;
 
-    friend.findByIdAndDelete(userId).then(result => {
+    friend.findOneAndDelete({ userId: frienderId, friendUserId: friendeeId }).then(result => {
         res.send(result);
     }).catch(err => {
-        console.log(err);
         res.send(err);
-    });
+    })
+}
+
+const friend_check = (req, res) => {
+    const frienderId = req.params.frienderId;
+    const friendeeId = req.params.friendeeId; 
+
+    friend.find({ userId: frienderId, friendUserId: friendeeId }).then(result => {
+        res.send(result);
+    }).catch(err => {
+        res.send(err);
+    })
 }
 
 
@@ -71,5 +141,7 @@ module.exports = {
     friend_index,
     friend_add,
     friend_list,
+    friend_list_users,
     friend_delete,
+    friend_check
 }
--- Locanotesgithub database/routes/friendRoutes.js
+++ Locanotesgithub database/routes/friendRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 router.post('/friend', friendController.friend_add);
 router.get('/friend', friendController.friend_index);
 router.get('/friend/:userId', friendController.friend_list);
-router.delete('/friend/:userId', friendController.friend_delete);
+router.get('/friend/users/:userId', friendController.friend_list_users);
+router.delete('/friend/:frienderId/:friendeeId', friendController.friend_delete);
+router.get('/friend/:frienderId/:friendeeId', friendController.friend_check);
 
 module.exports = router;
